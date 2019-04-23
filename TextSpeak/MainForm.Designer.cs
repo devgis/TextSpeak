2e6d6f118c6f07a3ae9f8c286310726df7f493d3
devgis <devgis@163.com>
2019-04-23 00:27:04 -0700

    TextSpeak
    TextSpeak

---
+++ TextSpeak/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TextSpeak
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btRead = new System.Windows.Forms.Button();
+            this.tbContent = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btRead, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbContent, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(763, 371);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btRead
+            // 
+            this.btRead.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btRead.Location = new System.Drawing.Point(673, 333);
+            this.btRead.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(87, 34);
+            this.btRead.TabIndex = 0;
+            this.btRead.Text = "阅读(&R)";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // tbContent
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.tbContent, 2);
+            this.tbContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbContent.Location = new System.Drawing.Point(3, 3);
+            this.tbContent.Multiline = true;
+            this.tbContent.Name = "tbContent";
+            this.tbContent.Size = new System.Drawing.Size(757, 323);
+            this.tbContent.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 371);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文本阅读";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btRead;
+        private System.Windows.Forms.TextBox tbContent;
+    }
+}
+
---
+++ TextSpeak/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace TextSpeak
+{
+    public partial class MainForm : Form
+    {
+        SpeechSynthesizer speech = null;
+        private int value = 100; //音量
+        private int rate=50; //语速
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            speech = new SpeechSynthesizer();
+            speech.Rate = rate;
+            speech.SelectVoice("Microsoft Lili");//设置播音员（中文）
+            //speech.SelectVoice("Microsoft Anna"); //英文
+            speech.Volume = value;
+        }
+
+        private void Speak()
+        {
+            btRead.Enabled = true;
+            
+            speech.SpeakAsync(tbContent.Text);//语音阅读方法
+            speech.SpeakCompleted += speech_SpeakCompleted;//绑定事件
+        }
+
+        void speech_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            btRead.Enabled = true;
+        }
+
+        /// <summary>
+        /// 生成语音文件的方法
+        /// </summary>
+        /// <param name="text"></param>
+        private void SaveFile(string text)
+        {
+            speech = new SpeechSynthesizer();
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "*.wav|*.wav|*.mp3|*.mp3";
+            dialog.ShowDialog();
+
+            string path = dialog.FileName;
+            if (path.Trim().Length == 0)
+            {
+                return;
+            }
+            speech.SetOutputToWaveFile(path);
+            speech.Volume = value;
+            speech.Rate = rate;
+            speech.Speak(text);
+            speech.SetOutputToNull();
+            MessageHelper.ShowInfo("生成成功!");
+
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbContent.Text))
+            {
+                MessageHelper.ShowError("内容不能为空！");
+            }
+            else
+            {
+                ThreadPool.QueueUserWorkItem(o =>
+                {
+                    this.Invoke((EventHandler)(delegate
+                    {
+                        Speak();
+                    }));
+                });
+            }
+        }
+    }
+}
